d6bc72644eb411c708a628106361240be5b76642
Roy Olav Purser <admin@royolav.net>
2016-04-06 21:16:00 +0200

    remove api-keys

--- technology/purser/ddns/Main.java
+++ technology/purser/ddns/Main.java
@@ -3,13 +3,6 @@ package technology.purser.ddns;
 class Main{
     public static void main(String[] args){
         
-        
-        /*
-            curl -X GET "https://api.cloudflare.com/client/v4/zones/" \                                             
-            -H "X-Auth-Email: spam@royolav.net" \
-            -H "X-Auth-Key: f3a64b8f9fe09f14e46f92c6f0952901b125a" \
-            -H "Content-Type: application/json" | json_reform
-        */
         System.out.println(expandIP("a:b::e:f:4:a"));
         
     }
